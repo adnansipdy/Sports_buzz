6a7cdee480bda0c2983050592eb4b1a3e743f9b7
adnansipdy <adnan7890786@gmail.com>
2023-01-27 21:33:10 +0530

    server update
    yooo

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+
+var yo=0;
 const searchFocus = document.getElementById('search-focus');
+ var  bad=0;
+$( "#badmintion" ).on({
+    
+    
+        
+        "click": function() {    
+        
+                  bad=1;
+                console.log(bad)
+
+    
+       yo=bad;
+    
+    }
+        
+
+    
+    
+    
+});
+
+
+console.log(bad);
+if(yo==1)
+{
+   $("#product_name").text("badmintion catogry"); 
+}
+
--- server.js
+++ server.js
@@ -5,22 +5,118 @@ const bodyparser=require("body-parser");
 const https=require("https");
 
 const app = express();
+
+const mongoose = require ('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/sprotsbuzz", {useNewUrlParser: true});
+
+const SportsSchema = new mongoose.Schema({
+name: String,
+Price:Number,
+Description : String
+});
+const Sport=mongoose.model("Sport", SportsSchema);
+
+const football = new Sport({
+name : "ball",
+Price:50,
+Description:"holly shit if this works "
+
+});
+
+const football1= new Sport({
+name : "bat",
+Price : 150,
+Description:"this should not work"
+
+});
+
+
+football.save();
+football1.save();
+
+
+
+
+
+Sport.find(function(err,sports){
+
+if(err){
+    console.log(err);
+
+}
+else
+{
+    for(i=0;i<sports.length;i++)
+    {
+        
+
+    console.log(sports[i].name);
+}
+
+}
+mongoose.connection.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.use(express.static("public"));
 
 
 app.use(bodyparser.urlencoded({extented:true}));
+
+app.use(express.static(__dirname));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 app.get("/",function(req,res){
 
 res.sendFile(__dirname+"/index.html");
-
+sadasd
 
 });
-
-
-
 
 
 app.listen(3000,function(res){
